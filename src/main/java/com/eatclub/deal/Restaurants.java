1fad8c6c76129a09a5da7ac8ac0316387b1ce9d3
john layton <johnstewartlayton@gmail.com>
2025-11-08 14:09:39 +1100

    Flesh out the restaurant model, including alias for deal open/close

--- src/main/java/com/eatclub/deal/Restaurants.java
+++ src/main/java/com/eatclub/deal/Restaurants.java
@@ -1,8 +1,40 @@
 package com.eatclub.deal;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.time.LocalTime;
 import java.util.List;
 
 public record Restaurants(List<Restaurant> restaurants) {
-    public record Restaurant(
-    ) {}
+    record Time(@JsonValue @JsonFormat(pattern = "h:mma") LocalTime value) implements Comparable<Time> {
+        @Override
+        public int compareTo(Time other) {
+            return value.compareTo(other.value());
+        }
+    }
+
+    record Deal(
+            String objectId,
+            int discount,
+            boolean dineIn,
+            boolean lightning,
+            @JsonAlias("start") Time open,
+            @JsonAlias("end") Time close,
+            int qtyLeft
+    ) {
+    }
+
+    record Restaurant(
+            String objectId,
+            String name,
+            String address1,
+            String suburb,
+            List<String> cuisines,
+            List<Deal> deals,
+            Time open,
+            Time close
+    ) {
+    }
 }
--- src/test/java/com/eatclub/deal/DealRepositoryLocalTest.java
+++ src/test/java/com/eatclub/deal/DealRepositoryLocalTest.java
@@ -1,12 +1,17 @@
 package com.eatclub.deal;
 
+import com.eatclub.deal.Restaurants.Deal;
+import com.eatclub.deal.Restaurants.Restaurant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(classes = DealRepositoryLocal.class)
 @AutoConfigureJson
@@ -19,7 +24,45 @@ class DealRepositoryLocalTest {
     @Test
     void shouldLoadLocalRepository() {
         Restaurants restaurants = dealRepository.getRestaurants();
-        assertEquals(6, restaurants.restaurants().size(), "Should load 6 restaurants from the local JSON file");
+        assertEquals(6, restaurants.restaurants().size(),
+                "Should load 6 restaurants from the local JSON file");
+
+        Restaurant restaurant = restaurants.restaurants().getFirst();
+        assertEquals("Masala Kitchen", restaurant.name(),
+                "The first restaurant should be Masala Kitchen");
+
+        Deal deal = restaurant.deals().getFirst();
+        assertEquals(50, deal.discount(),
+                "The first deal of the first restaurant should have a 50 discount");
+        assertFalse(deal.dineIn(),
+                "The first deal of the first restaurant should not be dine-in");
+        assertEquals(LocalTime.of(15, 0), deal.open().value(),
+                "The first deal of the first restaurant should open at 3:00pm");
     }
 
+    @Test
+    void shouldHandleJsonAliasForDealDuration() {
+        Restaurants restaurants = dealRepository.getRestaurants();
+
+        Restaurant restaurant = restaurants.restaurants().get(3);
+
+        assertEquals("Kekou", restaurant.name(),
+                "The first restaurant should be Kekou");
+
+        Deal deal1 = restaurant.deals().getFirst();
+
+        assertEquals(LocalTime.of(14, 0), deal1.open().value(),
+                "The first deal of the first restaurant should open at 2:00pm");
+
+        assertEquals(LocalTime.of(21, 0), deal1.close().value(),
+                "The first deal of the first restaurant should close at 9:00pm");
+
+        Deal deal2 = restaurant.deals().get(1);
+
+        assertEquals(LocalTime.of(17, 0), deal2.open().value(),
+                "The second deal of the first restaurant should open at 5:00pm");
+
+        assertEquals(LocalTime.of(21, 0), deal2.close().value(),
+                "The second deal of the first restaurant should close at 3:00pm");
+    }
 }
