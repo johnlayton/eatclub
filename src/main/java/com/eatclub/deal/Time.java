d439ea9a717892d161987c633dcb754fa73b652b
john layton <johnstewartlayton@gmail.com>
2025-11-14 11:28:54 +1100

    reduce visibility

--- src/main/java/com/eatclub/deal/Time.java
+++ src/main/java/com/eatclub/deal/Time.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.time.LocalTime;
 
-public record Time(@JsonValue LocalTime value) implements Comparable<Time> {
+public record Time(@JsonValue @JsonFormat(pattern = "h:mma") LocalTime value) implements Comparable<Time> {
     @Override
     public int compareTo(Time other) {
         return value.compareTo(other.value());
